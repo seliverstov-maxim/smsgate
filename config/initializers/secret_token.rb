f6c5dd98c8364748736cabba3db58e956eb05c16
vagrant <vagrant@precise64.(none)>
2013-07-29 16:30:23 +0000

    Init app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smsgetway::Application.config.secret_token = '6a64576ff5ac08bb9d626ac93aae847f3e64ce817c78da9045040df438bef14548bf6cb7557eb8abd8e67e15134f606d804125d1be6a3aae2ad81b182fede3f1'
