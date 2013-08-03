184696c46d814527626f4525b22bc3b076a37876
Maxim Seliverstov <maxim.web.developer@gmail.com>
2013-08-03 21:02:26 +0000

    Add test_helper, users test, user factories.

---
+++ test/functional/web/users_controller_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+
+class Web::UsersControllerTest < ActionController::TestCase
+  setup do
+    @user = create :user
+
+    user = create :user
+    sign_in user
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get show" do
+    get :show, {id: @user}
+    assert_response :success
+  end
+
+  test "should create user" do
+    params = {email: "test@email", password: "test"}
+    post :create, user: params
+
+    assert_response :redirect
+
+    user = User.find_by_email(params[:email])
+    assert_not_nil user
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, {id: @user.id}
+    assert_response :success
+  end
+
+  test "should update user" do
+    params = {email: "test@email", password: "test"}
+    
+    post :update, user: params, id: @user.id
+    assert_response :redirect
+    
+    @user.reload
+    assert_equal params[:email], @user.email
+  end
+
+  test "should destroy user" do
+    delete :destroy, {id: @user}
+    assert_response :redirect
+    
+    assert !User.exists?(@user)
+  end
+end
