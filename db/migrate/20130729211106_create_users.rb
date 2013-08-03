f1c496db647726d1759e79e75b33ab6e932fa4dc
Maxim Seliverstov <maxim.web.developer@gmail.com>
2013-08-03 22:11:45 +0000

    Modify migrate.

--- db/migrate/20130729211106_create_users.rb
+++ db/migrate/20130729211106_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :email
+      t.string :name
+      t.string :second_name
+      t.string :third_name
       t.string :api_key
       t.string :password_digest
 
